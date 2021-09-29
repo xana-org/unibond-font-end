f5c4d529b714834e9e8bfc4d19e1273d84821c38
boltsr <skrullweb.dev@gmail.com>
2021-09-28 20:20:58 -0400

    update second section

--- pages/index.js
+++ pages/index.js
@@ -22,25 +22,40 @@ const Home = () => {
           <Image src="/images/nft/10.png" mr="1%" alt="img8" borderRadius="5%"/>
         </Box>
       </Flex> */}
-      <Flex  maxW="80rem" w="100%" m="3rem auto" p="1rem  1rem" flexDirection="row" bg="#edf0f3">
-        <Box w='32%' mr="2%" bg="#FDFDFF" p="1rem  2rem" justifyContent='center' borderRadius='5%' alignItems='center' display='flex' flexDirection='column'>
-          <Image src="/images/icon/1.svg" mt="5%" alt="img8" />
+      <Flex  maxW="80rem" w="100%" m="3rem auto" p="1rem  1rem" flexDirection="row" >
+        <Box w='32%' mr="2%" bg="#FDFDFF" p="1rem  2rem" justifyContent='center' borderRadius='5%'display='flex' flexDirection='column'>
+          <Image src="/images/icon/4.svg" w="16px" mt="5%" alt="ico4" />
+          <Text fontWeight="bold" color="#000" mt="15%" fontSize="22px">Accurate</Text>
+          <Text fontWeight="regular" color="#a2a2ad" mt="5%" fontSize="15px">95+% accuracy, indoor coordinate accuracy at +/- 30 cm, surface temperature reading at +/- 0.5 C.</Text>
+        </Box>
+        <Box w='32%' mr="2%" bg="#FDFDFF" p="1rem  2rem" justifyContent='center' borderRadius='5%' display='flex' flexDirection='column'>
+          <Image src="/images/icon/5.svg" w="32px" mt="5%" alt="ico5" />
+          <Text fontWeight="bold" color="#000" mt="15%" fontSize="22px">Private by design</Text>
+          <Text fontWeight="regular" color="#a2a2ad" mt="5%" fontSize="15px">Our GDPR-compliant Heatic sensors use body heat to detect presence. We go where cameras can’t go. PII not possible.</Text>
+        </Box>
+        <Box w='32%' bg="#FDFDFF" p="1rem  2rem" justifyContent='center' borderRadius='5%' display='flex' flexDirection='column'>
+          <Image src="/images/icon/6.svg" w="32px" mt="5%" alt="ico6" />
+          <Text fontWeight="bold" color="#000" mt="15%" fontSize="22px">Wireless</Text>
+          <Text fontWeight="regular" color="#a2a2ad" mt="5%" fontSize="15px">Our magnetically mounted sensors run on a 2+ year battery life. No need for expensive cabling and complicated networking equipment on site.</Text>
+        </Box>
+      </Flex>
+      <Flex  maxW="80rem" w="100%" m="3rem auto" p="0rem  1rem" flexDirection="row" >
+        <Box w='32%' mr="2%" bg="#FDFDFF" p="1rem  2rem" justifyContent='center' borderRadius='5%'display='flex' flexDirection='column'>
+          <Image src="/images/icon/1.svg" w="32px" mt="5%" alt="ico1" />
           <Text fontWeight="bold" color="#000" mt="15%" fontSize="22px">Real-time</Text>
           <Text fontWeight="regular" color="#a2a2ad" mt="5%" fontSize="15px">With an update rate up to 7 fps, Heatic sensors provide real-time information in addition to historical data.</Text>
         </Box>
-        <Box w='32%' mr="2%" bg="#FDFDFF" p="1rem  2rem" justifyContent='center' borderRadius='5%' alignItems='center' display='flex' flexDirection='column'>
-          <Image src="/images/icon/2.svg" mt="5%" alt="img8" />
+        <Box w='32%' mr="2%" bg="#FDFDFF" p="1rem  2rem" justifyContent='center' borderRadius='5%' display='flex' flexDirection='column'>
+          <Image src="/images/icon/2.svg" w="32px" mt="5%" alt="ico2" />
           <Text fontWeight="bold" color="#000" mt="15%" fontSize="22px">Scalable</Text>
           <Text fontWeight="regular" color="#a2a2ad" mt="5%" fontSize="15px">Installs in minutes. Each Heatic sensor can cover up to 200 square feet depending on ceiling height.</Text>
         </Box>
-        <Box w='32%' bg="#FDFDFF" p="1rem  2rem" justifyContent='center' borderRadius='5%' alignItems='center' display='flex' flexDirection='column'>
-          <Image src="/images/icon/3.svg" mt="5%" alt="img8" />
+        <Box w='32%' bg="#FDFDFF" p="1rem  2rem" justifyContent='center' borderRadius='5%' display='flex' flexDirection='column'>
+          <Image src="/images/icon/3.svg" w="32px" mt="5%" alt="ico3" />
           <Text fontWeight="bold" color="#000" mt="15%" fontSize="22px">Extensible</Text>
           <Text fontWeight="regular" color="#a2a2ad" mt="5%" fontSize="15px">Systems integrators and developers can use our open APIs to integrate Heatic data into third party applications.</Text>
         </Box>
-
       </Flex>
-
     </Box>
   )
 }
